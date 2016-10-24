87fa9bf08dd188469ce5f28c4ad35169ce0038d6
Matheus Quintães <matheus.quintaes@gmail.com>
2016-10-24 10:48:15 -0200

    Finalizando filtro de nome

--- js/services/contatosAPIService.js
+++ js/services/contatosAPIService.js
@@ -1,22 +1,22 @@
 angular.module("listaTelefonica").factory("contatosAPI", function() {
 
   var objContatos = [
-    {nome: "Matheus" , data: new Date(), telefone: "12 997056221"},
-    {nome: "Pedro"   , data: new Date(), telefone: "16 997522311"},
-    {nome: "Adriana" , data: new Date(), telefone: "14 91298889" }
+    {nome: "matheus de castro" , data: new Date(), telefone: "12 997056221"},
+    {nome: "Pedro Quintães"   , data: new Date(), telefone: "16 997522311"},
+    {nome: "adriana pozenato" , data: new Date(), telefone: "14 91298889" }
   ];
 
   var _getContatos = function(){
     return objContatos;
-  }
+  };
 
   var _saveContato = function(contato){
     objContatos.push(contato);
-  }
+  };
 
   return {
     getContatos: _getContatos,
     saveContato: _saveContato
-  }
+  };
 
 });
